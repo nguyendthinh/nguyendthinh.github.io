97301cf1e6090a16f1522f9d4c158e15659ad26f
Thinh Nguyen <nguyendthinh@outlook.com>
2016-12-14 17:22:39 -0500

    more updates with styling in coding section

--- js/main.js
+++ js/main.js
@@ -2,38 +2,31 @@ $(document).ready(function(){
 
   $("#home").click(function(){
     $('main:not(#homepage)').css("display", "none");
+    $("html").css("background", "");
     $("#homepage").css("display", "flex");
   })
 
   $("#coding").click(function(){
     $('main:not(#codingpage)').css("display", "none");
+    $("html").css("background", "black");
     $("#codingpage").css("display", "block");
   })
 
   $("#design").click(function(){
     $('main:not(#designpage)').css("display", "none");
+    $("html").css("background", "black");
     $("#designpage").css("display", "block");
   })
 
   $("#about").click(function(){
     $('main:not(#aboutpage)').css("display", "none");
+    $("html").css("background", "");
     $("#aboutpage").css("display", "block");
   })
 
-  $("#project1").click(function(){
-    $("#isimonsays").slideToggle();
-  })
-
-  $("#project2").click(function(){
-    $("#decadefootwear").slideToggle();
-  })
-
-  $("#project3").click(function(){
-    $("#heyimbored").slideToggle();
-  })
-
-  $("#project4").click(function(){
-    $("#project4content").slideToggle();
+  $(".portfolioproject").click(function(){
+    $(this).toggleClass("active");
+    $(this).next().slideToggle();
   })
 
 });
