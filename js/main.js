9d08ffbb9522a24f0f246551322123a6c968d2e1
Thinh Nguyen <nguyendthinh@outlook.com>
2016-12-24 16:26:01 -0500

    more styling fixes

--- js/main.js
+++ js/main.js
@@ -2,31 +2,47 @@ $(document).ready(function(){
 
   $("#home").click(function(){
     $('main:not(#homepage)').css("display", "none");
-    $("html").css("background", "");
     $("#homepage").css("display", "flex");
   })
 
   $("#coding").click(function(){
     $('main:not(#codingpage)').css("display", "none");
-    $("html").css("background", "black");
     $("#codingpage").css("display", "block");
   })
 
   $("#design").click(function(){
     $('main:not(#designpage)').css("display", "none");
-    $("html").css("background", "black");
     $("#designpage").css("display", "block");
   })
 
   $("#about").click(function(){
     $('main:not(#aboutpage)').css("display", "none");
-    $("html").css("background", "");
     $("#aboutpage").css("display", "block");
   })
 
-  $(".portfolioproject").click(function(){
-    $(this).toggleClass("active");
-    $(this).next().slideToggle();
+  $("li").click(function(){
+    $(".active").next().slideToggle();
+    $(".active").toggleClass("active");
+  })
+
+  $(".portfolioproject").click(function(event){
+    event.preventDefault();
+    $a = $(this);
+
+      if ($(this).hasClass('active')) {
+        $(this).toggleClass("active");
+        $(this).next().slideToggle();
+      }
+      else {
+        $(".active").next().slideToggle();
+        $(".active").toggleClass("active")
+        $(this).toggleClass("active");
+        $(this).next().slideToggle(function(){
+          $('html, body').animate({
+            scrollTop: $a.offset().top
+          }, 'fast');
+        });
+      }
   })
 
 });
